7699131d6486c03797fe1201e33d39627f786e26
DarenOwl <DarenOwl@users.noreply.github.com>
2020-07-09 23:04:35 +0500

    Entity User added

---
+++ GenTreesCore/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace GenTreesCore.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+        public string Salt { get; set; }
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastVisited { get; set; }
+        public bool IsAdmin { get; set; }
+
+    }
+}
