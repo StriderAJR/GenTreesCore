d614d0b084ce7033a4bf15a431672fdcc8eaf12e
Alexander Kirsanov <alexander.a.kirsanov@gmail.com>
2020-07-10 23:28:20 +0500

    GT-0 Заготовка страницы с примером дерева

--- GenTreesCore/Controllers/HomeController.cs
+++ GenTreesCore/Controllers/HomeController.cs
@@ -34,5 +34,16 @@ namespace GenTreesCore.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult TestTree()
+        {
+            return View();
+        }
+
+        public JsonResult GetTestTree()
+        {
+            var genTree = DbProvider.GetTestGenTree();
+            return new JsonResult(genTree);
+        }
     }
 }
