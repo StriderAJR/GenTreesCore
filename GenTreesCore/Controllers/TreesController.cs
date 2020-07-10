32a322853d421200e299b511a305b500e7c5c3c5
DarenOwl <DarenOwl@users.noreply.github.com>
2020-07-10 21:55:06 +0500

    GT-13 Кнтроллеры  деревьев и view
    Запрос в бд  пока не работает

---
+++ GenTreesCore/Controllers/TreesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GenTreesCore.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GenTreesCore.Models;
+
+namespace GenTreesCore.Controllers
+{
+    public class TreesController : Controller
+    {
+        private ApplicationContext db;
+
+        public TreesController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PublicTrees()
+        {
+            var trees = db.GenTrees.FromSqlRaw("SELECT * FROM u0351346_GenTrees.u0351346_GenTrees.GenTrees")
+                .Where(tree => !tree.IsPrivate)
+                .ToList()
+                .Select(tree => new GenTreeListItemViewModel
+                {
+                    Name = tree.Name,
+                    Description = tree.Description,
+                    Author = tree.Owner.Login
+                });
+
+            return View(trees);
+        }
+    }
+}
---
+++ GenTreesCore/Models/GenTreeListItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenTreesCore.Models
+{
+    public class GenTreeListItemViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}
