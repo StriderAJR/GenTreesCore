3b188bdf7c438e3fc6a98d9f99d4d1d00debb81b
DarenOwl <DarenOwl@users.noreply.github.com>
2020-07-12 01:41:10 +0500

    GT-4 Очень простое тестовое дерево
    заполнено по минимуму

---
+++ GenTreesCore/Migrations/20200711201959_TestGenTree.cs
@@ -0,0 +1,28 @@
+﻿using GenTreesCore.Services;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GenTreesCore.Migrations
+{
+    public partial class TestGenTree : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                "GenTreeDateTimeSettings",
+                new string[] { "Id","Name", "Eras_json", "YearMonthCount" },
+                new object[] { 1,"TestSetting", "", 12 }
+            );
+            migrationBuilder.InsertData(
+                "GenTrees",
+                new string[] { "Id", "Name","OwnerId", "DateTimeSettingsId", "IsPrivate"},
+                new object[] { 1, "TestGenTree", 1, 1, false}
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData("GenTrees", "Id", 1);
+            migrationBuilder.DeleteData("GenTreeDateTimeSettings", "Id", 1);
+        }
+    }
+}
